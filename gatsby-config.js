79d871a8999f26cd89837da0dcc3f2572136cb2a
Ashley Sands <ashley.j.sands@gmail.com>
2021-03-21 08:06:16 +1000

    Update GA tracking id.

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-FP98H2NS84",
+        trackingId: "UA-192686853-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional

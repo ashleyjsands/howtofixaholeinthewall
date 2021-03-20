5cecf3b0bebd2c063641cc5116e7a8d62356d04f
Ashley Sands <ashley.j.sands@gmail.com>
2021-03-20 15:29:37 +1000

    Create first draft of the website. Add GA plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,71 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>The creation of holes</h1>
+    <p>Holes in the wall are created all the time. You're moving a big TV through a small corridor and you accidentally bang the wall. Or one of your kids rides his bike inside the house and decides to test our the strength of your gyrock walls. The unexpected price of having a family is the maitenance costs of your walls and now that you have more holes than a rabbit field, what are you are to do. You can either hire it out to a contractor, or you can always try fixing it yourself.</p>
+
+    <h1>How big is the hole?</h1>
+
+    <h2>Small hole (3 inches/~7cm or less)</h2>
+    <p>If the hole is 3 inches, or ~7cm, or less, then you can use a small hole repair kit.</p>
+    <p>Just purchase a small hole repair kit and follow the instructions.</p>
+
+    <h2>Big hole</h2>
+    <p>If the hole is bigger than 3 inches, then you will need to patch the hole properly with replacement plasterboard.</p>
+
+    <h3>1. In order to repair a large hole you will need:</h3>
+    <ul>
+      <li>a piece of sheetrock (plasterboard/gyrock) larger than the hole,</li>
+      <li>a piece of scrap wood,</li>
+      <li>some plaster screws,</li>
+      <li>joint tape,</li>
+      <li>some joint compound,</li>
+      <li>a drywall knife,</li>
+      <li>a sheetrock saw,</li>
+      <li>a pencil,</li>
+      <li>a knife,</li>
+      <li>and some sand paper (220 grit).</li>
+    </ul>
+
+    <h3>2. Cut up the sheetrock</h3>
+    <p>Now you need to cut up a piece of the sheet rock a little bit bigger than the hole in the wall. It just needs to be 10-20% bigger than the hole on all four sides.</p>
+    <p>With this particular technique, you don't need to straight or accurate cuts. The shape of the sheetrock doesn't need to be perfectly square either.</p>
+
+    <h3>3. Trace and cut out the hole in the wall</h3>
+    <p>Now that you have cut sheetrock, place it over the hole and trace out the shape of the sheetrock with a pencil. Now use the sheetrock saw to cut out the traced shape out of the wall.</p>
+
+    <h3>4. Install a wood backing inside of the hole.</h3>
+    <p>Cut up some scrap wood such that it can fit inside of the hole and be 
+      long enough to screw throught the wall into the top and bottom of the length of wood. 
+      This piece of wood would be used a backing to support the sheetrock 
+      that you will install. Simply install the piece of wood using two plaster 
+      screws.
     </p>
+
+    <h3>5. Apply the joint compound to the seam</h3>
+    <p>Fill in the gaps between the wall and the new sheetrock patch with the joint compound.</p>
+    <p>Be sure to smooth out and flatten the joint compound as much as possible.</p>
+
+    <h3>6. Apply the joint tape along all the sides of the patch.</h3>
+    <p>Completely flatten the tape into the mud (joint compound) and scrap away any access.</p>
+
+    <h3>7. Apply more joint compound, in a straigh line, over the joint tape.</h3>
+    <p>You may need to apply two coats to cover the all of the tape.</p>
+
+    <h3>8. Allow the joint compound to dry over night.</h3>
+    <p>You may need to apply multiple coats of joint compound to ensure that the surface is completely flat and smooth. Repeat as necessary.</p>
+
+    <h3>9. Sand the drywall with sandpaper.</h3>
+    <p>Sand the dried joint compound until it is smooth and relatively flat. Becareful to not over sand and expose any joint tape.</p>
+
+    <h3>10. Now paint the wall!</h3>
+    <p>You have successfully repaired your wall. You deserve a nice cold beer.</p>
+
   </Layout>
 )
 
